f8f3751fa18ced0dd4d883f0b8bc0277fddd3eb8
felipegaucho <felipegaucho>
2008-03-29 21:57:27 +0000

    ignores

---
+++ cejug-classifieds-server/test/functional/net/java/dev/cejug/classifieds/test/functional/FunctionalTestSuite.java
@@ -0,0 +1,9 @@
+package net.java.dev.cejug.classifieds.test.functional;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { LoadRssOperation.class })
+public class FunctionalTestSuite {
+}
