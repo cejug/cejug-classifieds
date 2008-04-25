aaeb7a92fd03830229992e5b15ca1ccc1b225ccf
felipegaucho <felipegaucho>
2008-04-25 20:58:42 +0000

    renaming

--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/server/reference/dao/ResponseTimeDao.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/server/reference/dao/ResponseTimeDao.java
@@ -2,55 +2,44 @@ package net.java.dev.cejug.classifieds.server.reference.dao;
 
 import java.util.List;
 
-import javax.persistence.EntityTransaction;
-
 import net.java.dev.cejug.classifieds.server.dao.AbstractClassifiedsServerDao;
-import net.java.dev.cejug.classifieds.server.generated.contract.ResponseTime;
-import net.java.dev.cejug.classifieds.server.reference.dao.pojos.ResponseTimeEntity;
+import net.java.dev.cejug.classifieds.server.generated.contract.OperationTimestamp;
 
-public class ResponseTimeDao extends AbstractClassifiedsServerDao<ResponseTime> {
-
-	public ResponseTimeDao() {
-		super();
-	}
+public class ResponseTimeDao extends
+		AbstractClassifiedsServerDao<OperationTimestamp> {
 
 	@Override
-	public ResponseTime create() throws Exception {
-
+	public OperationTimestamp create() throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void delete(ResponseTime type) throws Exception {
+	public void delete(OperationTimestamp type) throws Exception {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public List<ResponseTime> get(String query, int limit) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ResponseTime> getAll(int limit) throws Exception {
+	public List<OperationTimestamp> get(String query, int limit)
+			throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void update(ResponseTime source) throws Exception {
+	public List<OperationTimestamp> getAll(int limit) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-		// THIS ERROR AT GLASSFISH:
-		// <ns3:Text xml:lang="de">javax.persistence.PersistenceException: No
-		// Persistence provider for EntityManager named classifieds-ws-orm: No
-		// META-INF/persistence.xml was found in classpath.</ns3:Text>
+	@Override
+	public void update(OperationTimestamp type) throws Exception {
 
-		ResponseTimeEntity entity = new ResponseTimeEntity(source);
-		EntityTransaction transaction = manager.getTransaction();
-		transaction.begin();
-		manager.persist(entity);
-		transaction.commit();
+		/*
+		 * ResponseTimeEntity entity = new ResponseTimeEntity(source);
+		 * EntityTransaction transaction = manager.getTransaction();
+		 * transaction.begin(); manager.persist(entity); transaction.commit();
+		 */
 	}
 }
