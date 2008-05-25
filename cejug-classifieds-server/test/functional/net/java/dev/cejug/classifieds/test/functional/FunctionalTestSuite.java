3a48e8bce17b6fd2943cdd59ce35302310f8b95e
felipegaucho <felipegaucho>
2008-05-25 07:08:53 +0000

    splitting the WSDL in Business X Admin

--- cejug-classifieds-server/test/functional/net/java/dev/cejug/classifieds/test/functional/FunctionalTestSuite.java
+++ cejug-classifieds-server/test/functional/net/java/dev/cejug/classifieds/test/functional/FunctionalTestSuite.java
@@ -6,6 +6,6 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { LoadAtomFunctionalTest.class,
 		LoadRssFunctionalTest.class, PublishFunctionalTest.class,
-		ReportSpamFunctionalTest.class })
+		ReportSpamFunctionalTest.class, AdminFunctionalTest.class })
 public class FunctionalTestSuite {
 }
