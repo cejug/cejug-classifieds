fcaaa4f4f20a12ed5b1eb50353d01fec21613185
rodrigolopes <rodrigolopes>
2008-06-08 10:50:25 +0000

    adjsuting database mapping and entities

---
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/server/ejb3/entity/AbstractEntity.java
@@ -0,0 +1,34 @@
+/*
+ * Entity.java Created on 19 de Setembro de 2000, 10:15
+ */
+package net.java.dev.cejug.classifieds.server.ejb3.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Entity Class.
+ * 
+ * @author Rodrigo Lopes
+ * @version 1.0
+ * @since 08/06/2008
+ */
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Integer id;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+
+        return id;
+    }
+}
---
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/server/ejb3/entity/AdvertisementKeywordEntity.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package net.java.dev.cejug.classifieds.server.ejb3.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author rodrigo
+ * 
+ */
+@Entity
+@Table(name = "KEYWORD")
+public class AdvertisementKeywordEntity extends AbstractEntity {
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+}
