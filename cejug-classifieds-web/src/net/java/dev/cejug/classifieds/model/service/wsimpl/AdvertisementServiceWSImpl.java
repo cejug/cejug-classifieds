7a3f5f00779aae4cb6c2653279dcf0978a904841
felipegaucho <felipegaucho>
2008-06-15 13:20:57 +0000

    stubs-generator task

--- cejug-classifieds-web/src/net/java/dev/cejug/classifieds/model/service/wsimpl/AdvertisementServiceWSImpl.java
+++ cejug-classifieds-web/src/net/java/dev/cejug/classifieds/model/service/wsimpl/AdvertisementServiceWSImpl.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import net.java.dev.cejug.classifieds.model.entity.Advertisement;
 import net.java.dev.cejug.classifieds.model.service.AdvertisementService;
+import net.java.dev.cejug.classifieds.server.generated.contract.Advertisement;
+
 /**
- * This implementation is responsible for the communication with the web-services stubs
- * of the classifieds server.
+ * This implementation is responsible for the communication with the
+ * web-services stubs of the classifieds server.
+ * 
  * @author Tarso Bessa
- *
+ * 
  */
 public class AdvertisementServiceWSImpl implements AdvertisementService {
 	/**
-	 * This field is for test purpose only. 
+	 * This field is for test purpose only.
 	 */
 	private List<Advertisement> ads = new ArrayList<Advertisement>();
-	
+
 	@Override
 	public Advertisement publish(Advertisement advertisement) {
-		advertisement.setId((int)(Math.random()*1000000));
+		// TODO: ID should be included in the Advertisement element.
+		// advertisement.setId((int) (Math.random() * 1000000));
 		ads.add(advertisement);
 		return advertisement;
 	}
@@ -29,8 +32,5 @@ public class AdvertisementServiceWSImpl implements AdvertisementService {
 	public List<Advertisement> getAll() {
 		return Collections.unmodifiableList(ads);
 	}
-	
-	
-	
 
 }
