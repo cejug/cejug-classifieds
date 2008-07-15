5cbe872f80562652bba28109d1169829c500a2a1
rodrigolopes <rodrigolopes>
2008-07-15 20:10:01 +0000

    operations names consistent to session beans operations names

--- cejug-classifieds-admin/java/src/net/java/dev/cejug/classifieds/admin/service/CejugClassifiedsAdminService.java
+++ cejug-classifieds-admin/java/src/net/java/dev/cejug/classifieds/admin/service/CejugClassifiedsAdminService.java
@@ -10,13 +10,18 @@ import net.java.dev.cejug.classifieds.server.contract.CejugClassifiedsAdmin;
 import net.java.dev.cejug.classifieds.server.contract.CejugClassifiedsServiceAdmin;
 import net.java.dev.cejug.classifieds.server.contract.CreateAdvertisementTypeParam;
 import net.java.dev.cejug.classifieds.server.contract.CreateCategoryParam;
+import net.java.dev.cejug.classifieds.server.contract.CreateDomainParam;
 import net.java.dev.cejug.classifieds.server.contract.DeleteAdvertisementTypeParam;
 import net.java.dev.cejug.classifieds.server.contract.DeleteCategoryParam;
+import net.java.dev.cejug.classifieds.server.contract.DeleteDomainParam;
+import net.java.dev.cejug.classifieds.server.contract.Domain;
+import net.java.dev.cejug.classifieds.server.contract.DomainCollection;
 import net.java.dev.cejug.classifieds.server.contract.ReadAdvertisementTypeBundleParam;
 import net.java.dev.cejug.classifieds.server.contract.ReadCategoryBundleParam;
 import net.java.dev.cejug.classifieds.server.contract.ServiceStatus;
 import net.java.dev.cejug.classifieds.server.contract.UpdateAdvertisementTypeParam;
 import net.java.dev.cejug.classifieds.server.contract.UpdateCategoryParam;
+import net.java.dev.cejug.classifieds.server.contract.UpdateDomainParam;
 
 /**
  * @author rodrigo
@@ -33,7 +38,7 @@ public class CejugClassifiedsAdminService {
      * @param param Params to search the categories
      * @return List of categories
      */
-    public List<AdvertisementCategory> readAllCategories(ReadCategoryBundleParam param) {
+    public List<AdvertisementCategory> readCategoryBundleOperation(ReadCategoryBundleParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.readCategoryBundleOperation(param).getAdvertisementCategory();
@@ -44,7 +49,7 @@ public class CejugClassifiedsAdminService {
      * @param param Category to be created.
      * @return Status about the create operation.
      */
-    public ServiceStatus createCategory(CreateCategoryParam param) {
+    public ServiceStatus createCategoryOperation(CreateCategoryParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.createCategoryOperation(param);
@@ -55,7 +60,7 @@ public class CejugClassifiedsAdminService {
      * @param param Category to be updated.
      * @return Status about the update operation.
      */
-    public ServiceStatus updateCategory(UpdateCategoryParam param) {
+    public ServiceStatus updateCategoryOperation(UpdateCategoryParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.updateCategoryOperation(param);
@@ -66,7 +71,7 @@ public class CejugClassifiedsAdminService {
      * @param param Information to delete a category
      * @return Status about the update operation.
      */
-    public ServiceStatus deleteCategory(DeleteCategoryParam param) {
+    public ServiceStatus deleteCategoryOperation(DeleteCategoryParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.deleteCategoryOperation(param);
@@ -82,7 +87,7 @@ public class CejugClassifiedsAdminService {
      * @param param Params to search the advertisement types
      * @return List of advertisement types
      */
-    public List<AdvertisementType> readAllAdvTypes(ReadAdvertisementTypeBundleParam param) {
+    public List<AdvertisementType> readAdvertisementTypeBundleOperation(ReadAdvertisementTypeBundleParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.readAdvertisementTypeBundleOperation(param).getAdvertisementType();
@@ -93,7 +98,7 @@ public class CejugClassifiedsAdminService {
      * @param param Advertisement type to be created.
      * @return Status about the create operation.
      */
-    public ServiceStatus createAdvType(CreateAdvertisementTypeParam param) {
+    public ServiceStatus createAdvertisementTypeOperation(CreateAdvertisementTypeParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.createAdvertisementTypeOperation(param);
@@ -104,7 +109,7 @@ public class CejugClassifiedsAdminService {
      * @param param Advertisement type to be updated.
      * @return Status about the update operation.
      */
-    public ServiceStatus updateAdvType(UpdateAdvertisementTypeParam param) {
+    public ServiceStatus updateAdvertisementTypeOperation(UpdateAdvertisementTypeParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.updateAdvertisementTypeOperation(param);
@@ -115,9 +120,59 @@ public class CejugClassifiedsAdminService {
      * @param param Information to delete an advertisement type
      * @return Status about the delete operation.
      */
-    public ServiceStatus deleteAdvType(DeleteAdvertisementTypeParam param) {
+    public ServiceStatus deleteAdvertisementTypeOperation(DeleteAdvertisementTypeParam param) {
 
         CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
         return admin.deleteAdvertisementTypeOperation(param.getPrimaryKey());
+    }
+
+    /*
+     * =================================================================================
+     * DOMAIN
+     * ==================================================================================
+     */
+    /**
+     * Get all domains.
+     * @param param Params to search the domains
+     * @return List of domains
+     */
+    public List<Domain> readDomainBundleOperation() {
+
+        CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
+        DomainCollection c = admin.readDomainBundleOperation();
+        return c.getDomain();
+    }
+
+    /**
+     * Creates a new domain.
+     * @param param Domain to be created.
+     * @return Status about the create operation.
+     */
+    public ServiceStatus createDomainOperation(CreateDomainParam param) {
+
+        CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
+        return admin.createDomainOperation(param);
+    }
+
+    /**
+     * Updates a domain.
+     * @param param Domain to be updated.
+     * @return Status about the update operation.
+     */
+    public ServiceStatus updateDomainOperation(UpdateDomainParam param) {
+
+        CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
+        return admin.updateDomainOperation(param);
+    }
+
+    /**
+     * delete one domain.
+     * @param param Information to delete a domain
+     * @return Status about the update operation.
+     */
+    public ServiceStatus deleteDomainOperation(DeleteDomainParam param) {
+
+        CejugClassifiedsAdmin admin = new CejugClassifiedsServiceAdmin().getCejugClassifiedsAdmin();
+        return admin.deleteDomainOperation(param);
     }
 }
