cb3321c8903a2968ac32640334b42bc398cad4b9
felipegaucho <felipegaucho>
2008-09-02 08:42:07 +0000

    adapters experiment (under evaluation)

---
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/server/ejb3/entity/adapter/SoapEntityAdapter.java
@@ -0,0 +1,61 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ Copyright (C) 2008 CEJUG - Ceará Java Users Group
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ 
+ This file is part of the CEJUG-CLASSIFIEDS Project - an  open source classifieds system
+ originally used by CEJUG - Ceará Java Users Group.
+ The project is hosted https://cejug-classifieds.dev.java.net/
+ 
+ You can contact us through the mail dev@cejug-classifieds.dev.java.net
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+package net.java.dev.cejug.classifieds.server.ejb3.entity.adapter;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Retention(RUNTIME)
+@Target({PACKAGE, FIELD, METHOD, TYPE, PARAMETER})
+public @interface SoapEntityAdapter {
+
+  /**
+   * Points to the class that converts a value type to a bound type or vice
+   * versa. See {@link SoapAdapter} for more details.
+   */
+  Class<? extends SoapAdapter<?, ?>> value();
+
+  /**
+   * If this annotation is used at the package level, then value of the type()
+   * must be specified.
+   */
+
+  Class<?> type() default DEFAULT.class;
+
+  /**
+   * Used in {@link SoapEntityAdapter#type()} to signal that the type be
+   * inferred from the signature of the field, property, parameter or the class.
+   */
+
+  static final class DEFAULT {
+  }
+}
