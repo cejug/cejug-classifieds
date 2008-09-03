fdc5a3886f5f5a9b10b56185d3a912f15a318af0
rodrigolopes <rodrigolopes>
2008-09-03 19:40:45 +0000

    implementing methods

--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/AdvertisementTypeAdapter.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/AdvertisementTypeAdapter.java
@@ -3,20 +3,29 @@ package net.java.dev.cejug.classifieds.adapter;
 import net.java.dev.cejug.classifieds.entity.AdvertisementTypeEntity;
 import net.java.dev.cejug_classifieds.metadata.common.AdvertisementType;
 
-public class AdvertisementTypeAdapter extends
-		SoapOrmAdapter<AdvertisementType, AdvertisementTypeEntity> {
+public class AdvertisementTypeAdapter extends SoapOrmAdapter<AdvertisementType, AdvertisementTypeEntity> {
 
-	@Override
-	public AdvertisementTypeEntity toEntity(AdvertisementType type)
-			throws IllegalStateException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public AdvertisementTypeEntity toEntity(AdvertisementType type) throws IllegalStateException, IllegalArgumentException {
 
-	@Override
-	public AdvertisementType toSoap(AdvertisementTypeEntity type)
-			throws IllegalStateException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        AdvertisementTypeEntity advTypeEntity = new AdvertisementTypeEntity();
+
+        advTypeEntity.setDescription(type.getDescription());
+        advTypeEntity.setMaxAttachmentSize(type.getMaxAttachmentSize());
+        advTypeEntity.setName(type.getName());
+        advTypeEntity.setTextLength(type.getMaxTextLength());
+        return advTypeEntity;
+    }
+
+    @Override
+    public AdvertisementType toSoap(AdvertisementTypeEntity advTypeEntity) throws IllegalStateException, IllegalArgumentException {
+
+        AdvertisementType advType = new AdvertisementType();
+        advType.setId((int) advTypeEntity.getId());
+        advType.setDescription(advTypeEntity.getDescription());
+        advType.setName(advTypeEntity.getName());
+        advType.setMaxAttachmentSize(advTypeEntity.getMaxAttachmentSize());
+        advType.setMaxTextLength(advTypeEntity.getTextLength());
+        return advType;
+    }
 }
