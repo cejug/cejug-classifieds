cab88f375b74ea570722d381788f2e730c2f7ffb
rodrigolopes <rodrigolopes>
2008-09-03 20:01:08 +0000

    implementing methods

--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/CategoryAdapter.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/CategoryAdapter.java
@@ -3,20 +3,35 @@ package net.java.dev.cejug.classifieds.adapter;
 import net.java.dev.cejug.classifieds.entity.CategoryEntity;
 import net.java.dev.cejug_classifieds.metadata.common.AdvertisementCategory;
 
-public class CategoryAdapter extends
-		SoapOrmAdapter<AdvertisementCategory, CategoryEntity> {
+public class CategoryAdapter extends SoapOrmAdapter<AdvertisementCategory, CategoryEntity> {
 
-	@Override
-	public CategoryEntity toEntity(AdvertisementCategory type)
-			throws IllegalStateException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public CategoryEntity toEntity(AdvertisementCategory advCategory) throws IllegalStateException, IllegalArgumentException {
 
-	@Override
-	public AdvertisementCategory toSoap(CategoryEntity type)
-			throws IllegalStateException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        CategoryEntity category = new CategoryEntity();
+        category.setId(advCategory.getEntityId());
+        category.setDescription(advCategory.getDescription());
+        category.setName(advCategory.getName());
+
+        AdvertisementCategory parent = advCategory.getAdvertisementCategory();
+        if (parent != null) {
+            category.setParent(toEntity(parent));
+        }
+        return category;
+    }
+
+    @Override
+    public AdvertisementCategory toSoap(CategoryEntity entity) throws IllegalStateException, IllegalArgumentException {
+
+        AdvertisementCategory cat = new AdvertisementCategory();
+        cat.setEntityId(entity.getId());
+        cat.setDescription(entity.getDescription());
+        cat.setName(entity.getName());
+        cat.setAvailable(entity.getAvailable());
+
+        if (entity.getParent() != null) {
+            cat.setAdvertisementCategory(toSoap(entity.getParent()));
+        }
+        return cat;
+    }
 }
--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/DomainAdapter.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/DomainAdapter.java
@@ -1,21 +1,50 @@
 package net.java.dev.cejug.classifieds.adapter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import net.java.dev.cejug.classifieds.entity.CategoryEntity;
 import net.java.dev.cejug.classifieds.entity.DomainEntity;
+import net.java.dev.cejug_classifieds.metadata.common.AdvertisementCategory;
 import net.java.dev.cejug_classifieds.metadata.common.Domain;
 
 public class DomainAdapter extends SoapOrmAdapter<Domain, DomainEntity> {
 
-	@Override
-	public DomainEntity toEntity(Domain type) throws IllegalStateException,
-			IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public DomainEntity toEntity(Domain domain) throws IllegalStateException, IllegalArgumentException {
 
-	@Override
-	public Domain toSoap(DomainEntity type) throws IllegalStateException,
-			IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        DomainEntity domainEntity = new DomainEntity();
+
+        domainEntity.setDomainName(domain.getUri());
+        domainEntity.setSharedQuota(domain.isSharedQuota());
+        domainEntity.setBrand(domain.getBrand());
+        Collection<CategoryEntity> categories = new ArrayList<CategoryEntity>();
+        if (domain.getAdvertisementCategory() != null) {
+            CategoryAdapter categoryAdapter = new CategoryAdapter();
+            for (AdvertisementCategory category : domain.getAdvertisementCategory()) {
+                CategoryEntity categoryEntity = categoryAdapter.toEntity(category);
+                categories.add(categoryEntity);
+            }
+        }
+        domainEntity.setCategories(categories);
+        return domainEntity;
+    }
+
+    @Override
+    public Domain toSoap(DomainEntity domainEntity) throws IllegalStateException, IllegalArgumentException {
+
+        Domain domain = new Domain();
+        domain.setEntityId(domainEntity.getId());
+        domain.setBrand(domainEntity.getBrand());
+        domain.setUri(domainEntity.getDomainName());
+        domain.setSharedQuota(domainEntity.getSharedQuota());
+
+        if (domainEntity.getCategories() != null) {
+            CategoryAdapter categoryAdapter = new CategoryAdapter();
+            for (CategoryEntity categoryEntity : domainEntity.getCategories()) {
+                AdvertisementCategory category = categoryAdapter.toSoap(categoryEntity);
+                domain.getAdvertisementCategory().add(category);
+            }
+        }
+        return domain;
+    }
 }
