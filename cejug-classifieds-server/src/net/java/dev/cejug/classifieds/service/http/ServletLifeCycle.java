68726ca2aab84585eee8e062927e0cd2d87208f3
felipegaucho <felipegaucho>
2008-09-05 13:23:28 +0000

    javadoc cleanup

--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/service/http/ServletLifeCycle.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/service/http/ServletLifeCycle.java
@@ -1,17 +1,26 @@
-/*
- * @(#) $CVSHeader:  $
- *
- * Copyright (C) 2008 by Netcetera AG.
- * All rights reserved.
- *
- * The copyright to the computer program(s) herein is the property of
- * Netcetera AG, Switzerland.  The program(s) may be used and/or copied
- * only with the written permission of Netcetera AG or in accordance
- * with the terms and conditions stipulated in the agreement/contract
- * under which the program(s) have been supplied.
- *
- * @(#) $Id$
- */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ Copyright (C) 2008 CEJUG - Ceará Java Users Group
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ 
+ This file is part of the CEJUG-CLASSIFIEDS Project - an  open source classifieds system
+ originally used by CEJUG - Ceará Java Users Group.
+ The project is hosted https://cejug-classifieds.dev.java.net/
+ 
+ You can contact us through the mail dev@cejug-classifieds.dev.java.net
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 package net.java.dev.cejug.classifieds.service.http;
 
 import java.util.Calendar;
