5640cabf86ea83822121022b41189dbaa9551c83
arisson <arisson>
2008-11-08 21:52:08 +0000

    publish advertisement with image upload

--- cejug-classifieds-richfaces/src/net/java/dev/cejug/classifieds/richfaces/view/PublishAdvertisementBean.java
+++ cejug-classifieds-richfaces/src/net/java/dev/cejug/classifieds/richfaces/view/PublishAdvertisementBean.java
@@ -1,17 +1,25 @@
 package net.java.dev.cejug.classifieds.richfaces.view;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.faces.model.SelectItem;
 
+import net.java.dev.cejug_classifieds.metadata.attachments.AtavarImage;
+import net.java.dev.cejug_classifieds.metadata.attachments.AvatarImageOrUrl;
 import net.java.dev.cejug_classifieds.metadata.business.Advertisement;
 
+import org.richfaces.event.UploadEvent;
+import org.richfaces.model.UploadItem;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 @Controller(value = "publishAdvertisementBean")
-@Scope("request")
+@Scope("session")
 public class PublishAdvertisementBean {
 	private Advertisement advertisement = new Advertisement();
 
@@ -39,4 +47,89 @@ public class PublishAdvertisementBean {
 		list.add(new SelectItem("U", "Url"));
 		return list;
 	}
+
+	public List<SelectItem> getLocales() {
+		List<SelectItem> locales = new ArrayList<SelectItem>();
+
+		Locale list[] = SimpleDateFormat.getAvailableLocales();
+
+		for (int i = 0; i < list.length; i++) {
+			locales.add(new SelectItem(list[i].toString(), list[i]
+					.getDisplayName(Locale.US)));
+		}
+
+		return locales;
+	}
+	
+	/* File Upload */
+	private ArrayList<AtavarImage> files = new ArrayList<AtavarImage>();
+	private int uploadsAvailable = 1;
+	private boolean autoUpload = true;
+	private boolean useFlash = false;
+	public int getSize() {
+		if (getFiles().size()>0){
+			return getFiles().size();
+		}else 
+		{
+			return 0;
+		}
+	}
+
+	
+	public synchronized void paint(OutputStream stream, Object object) throws IOException {
+		stream.write(getFiles().get((Integer)object).getValue());
+	}
+	public synchronized void listener(UploadEvent event) throws Exception{
+	    UploadItem item = event.getUploadItem();
+	    
+	    advertisement.setAvatarImageOrUrl(new AvatarImageOrUrl());
+	    advertisement.getAvatarImageOrUrl().setImage(new AtavarImage());
+	    advertisement.getAvatarImageOrUrl().getImage().setValue(item.getData());
+	    
+	    files.add(advertisement.getAvatarImageOrUrl().getImage());
+	    uploadsAvailable--;
+	}
+	
+	public String clearUploadData() {
+		files.clear();
+		setUploadsAvailable(5);
+		return null;
+	}
+	
+	public long getTimeStamp(){
+		return System.currentTimeMillis();
+	}
+	
+	public ArrayList<AtavarImage> getFiles() {
+		return files;
+	}
+
+	public void setFiles(ArrayList<AtavarImage> files) { 
+		this.files = files;
+	}
+
+	public int getUploadsAvailable() {
+		return uploadsAvailable;
+	}
+
+	public void setUploadsAvailable(int uploadsAvailable) {
+		this.uploadsAvailable = uploadsAvailable;
+	}
+
+	public boolean isAutoUpload() {
+		return autoUpload;
+	}
+
+	public void setAutoUpload(boolean autoUpload) {
+		this.autoUpload = autoUpload;
+	}
+
+	public boolean isUseFlash() {
+		return useFlash;
+	}
+
+	public void setUseFlash(boolean useFlash) {
+		this.useFlash = useFlash;
+	}
+
 }
