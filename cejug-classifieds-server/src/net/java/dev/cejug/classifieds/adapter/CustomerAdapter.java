f4303c932a6c3432d3a3e4bf0fd2206040c5c6e6
rodrigolopes <rodrigolopes>
2008-12-06 13:43:51 +0000

    fix the return of the toSoap method

--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/CustomerAdapter.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/adapter/CustomerAdapter.java
@@ -25,7 +25,6 @@ package net.java.dev.cejug.classifieds.adapter;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-
 import net.java.dev.cejug.classifieds.business.interfaces.CustomerAdapterLocal;
 import net.java.dev.cejug.classifieds.entity.CustomerEntity;
 import net.java.dev.cejug.classifieds.entity.facade.DomainFacadeLocal;
@@ -34,34 +33,33 @@ import net.java.dev.cejug_classifieds.metadata.common.Customer;
 /**
  * Adaptation interface between Customer objects representing Soap elements and
  * the CustomerEntity used to persist the domain data in the database.
- * 
  * @author $Author$
  * @version $Rev$ ($Date$)
  */
 @Stateless
 public class CustomerAdapter implements CustomerAdapterLocal {
 
-	@EJB
-	private transient DomainFacadeLocal domainFacade;
+    @EJB
+    private transient DomainFacadeLocal domainFacade;
 
-	/** {@inheritDoc} */
-	public CustomerEntity toEntity(Customer customer)
-			throws IllegalStateException, IllegalArgumentException {
-		CustomerEntity entity = new CustomerEntity();
-		entity.setDomain(domainFacade.read(customer.getDomainId()));
-		entity.setId(customer.getEntityId());
-		entity.setLogin(customer.getLogin());
-		// entity.setQuotas(type.getEntityId());
-		return entity;
-	}
+    /** {@inheritDoc} */
+    public CustomerEntity toEntity(Customer customer) throws IllegalStateException, IllegalArgumentException {
 
-	/** {@inheritDoc} */
-	public Customer toSoap(CustomerEntity entity) throws IllegalStateException,
-			IllegalArgumentException {
-		Customer customer = new Customer();
-		customer.setDomainId(entity.getDomain().getId());
-		customer.setEntityId(entity.getId());
-		customer.setLogin(entity.getLogin());
-		return null;
-	}
+        CustomerEntity entity = new CustomerEntity();
+        entity.setDomain(domainFacade.read(customer.getDomainId()));
+        entity.setId(customer.getEntityId());
+        entity.setLogin(customer.getLogin());
+        // entity.setQuotas(type.getEntityId());
+        return entity;
+    }
+
+    /** {@inheritDoc} */
+    public Customer toSoap(CustomerEntity entity) throws IllegalStateException, IllegalArgumentException {
+
+        Customer customer = new Customer();
+        customer.setDomainId(entity.getDomain().getId());
+        customer.setEntityId(entity.getId());
+        customer.setLogin(entity.getLogin());
+        return customer;
+    }
 }
