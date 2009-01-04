2651b825fed6a4a1cc3c0e0f324fb36963ed1238
felipegaucho <felipegaucho>
2009-01-04 16:54:47 +0000

    down grading the severity of i18n checker to LOW

--- cejug-classifieds-server/src/net/java/dev/cejug/classifieds/service/http/ServletLifeCycle.java
+++ cejug-classifieds-server/src/net/java/dev/cejug/classifieds/service/http/ServletLifeCycle.java
@@ -1,5 +1,5 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- Copyright (C) 2008 CEJUG - Ceará Java Users Group
+ Copyright (C) 20087-2009 CEJUG - Ceará Java Users Group
  
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
