6a62b849090b52b1221a005c3a5c96e199c10503
felipegaucho <felipegaucho>
2009-02-22 17:46:22 +0000

    first draft encryption

--- cejug-classifieds-login-rest/src/main/java/net/java/dev/cejug/classifieds/login/resource/RegistrationConfirmation.java
+++ cejug-classifieds-login-rest/src/main/java/net/java/dev/cejug/classifieds/login/resource/RegistrationConfirmation.java
@@ -2,7 +2,10 @@ package net.java.dev.cejug.classifieds.login.resource;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Locale;
+import java.security.GeneralSecurityException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -10,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.java.dev.cejug.classifieds.login.entity.facade.client.RegistrationConstants;
+import net.java.dev.cejug.classifieds.login.entity.facade.client.URLDeobfuscator;
 import net.java.dev.cejug.classifieds.login.entity.facade.client.UserFacadeLocal;
 
 public class RegistrationConfirmation extends HttpServlet {
@@ -19,13 +24,41 @@ public class RegistrationConfirmation extends HttpServlet {
 	@EJB
 	private UserFacadeLocal local;
 
+	@EJB
+	private URLDeobfuscator deobfuscator;
+
 	/** {@inheritDoc} */
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		PrintWriter out = response.getWriter();
-		out.print("@EJB UserFacadeLocal ref = " + local);
+		String key = request
+				.getParameter(RegistrationConstants.CONFIRMATION_KEY.value());
+		if (key == null) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					"invalid key - goodbye hacker...");
+		} else {
+			try {
+				Map<String, String> parameters = deobfuscator
+						.extractParameters(key);
+				PrintWriter out = response.getWriter();
+				Set<Map.Entry<String, String>> keys = parameters.entrySet();
+
+				for (Iterator<Map.Entry<String, String>> it = keys.iterator(); it
+						.hasNext();) {
+					Map.Entry<String, String> entry = it.next();
+					out.print("<strong>");
+					out.print(entry.getKey());
+					out.print("</strong> :");
+					out.print(entry.getValue());
+					out.print("<br>");
+				}
+
+			} catch (GeneralSecurityException e) {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, e
+						.getMessage());
+			}
+		}
 	}
 
 	/** {@inheritDoc} */
